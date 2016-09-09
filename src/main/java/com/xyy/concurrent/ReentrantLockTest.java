2e2328e38b802bbec6e71a431e7d20da7a3ab594
xuyuanye <xuyuanye@bestpay.com.cn>
2016-09-09 10:28:38 +0800

    update

--- src/main/java/com/xyy/concurrent/ReentrantLockTest.java
+++ src/main/java/com/xyy/concurrent/ReentrantLockTest.java
@@ -2,6 +2,7 @@ package com.xyy.concurrent;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -13,57 +14,61 @@ import java.util.concurrent.locks.ReentrantLock;
 public class ReentrantLockTest {
 
     public static void main(String[] args){
-        final ReentrantLock lock=new ReentrantLock();
+        final ReentrantLock lock = new ReentrantLock();
         final Condition notFull = lock.newCondition();
         final Condition notEmpty = lock.newCondition();
-        final List<Bread> basket = new ArrayList<Bread>(10);
+        final List<Bread> basket = new ArrayList(10);
 
-        new Thread(new Runnable() {
-            public void run() {
-                int i=0;
-                while(true){
-                    lock.lock();
-                    try {
-                        while(basket.size()==10){
-                            notFull.await();
+        for(int k=0;k<5;k++){
+            new Thread(new Runnable() {
+                public void run() {
+                    int i=0;
+                    while(true){
+                        lock.lock();
+                        try {
+                            while(basket.size()==10){
+                                notFull.await();
+                            }
+                            Bread bread = new Bread(i++);
+                            basket.add(bread);
+                            System.out.println(Thread.currentThread().getName()+"add:"+bread);
+                            notEmpty.signalAll();
+                        }catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }finally {
+                            lock.unlock();
                         }
-                        Bread bread = new Bread(i++);
-                        basket.add(bread);
-                        System.out.println("add:"+bread);
-                        notEmpty.signalAll();
-                    }catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }finally {
-                        lock.unlock();
-                    }
-                 /*   try {
+                   try {
                         TimeUnit.SECONDS.sleep(1);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
-                    }*/
-                }
-            }
-        }).start();
-
-        new Thread(new Runnable() {
-            public void run() {
-                while(true){
-                    lock.lock();
-                    try {
-                        while(basket.size()==0){
-                            notEmpty.await();
-                        }
-                        Bread bread = basket.remove(0);
-                        System.out.println("remove:"+bread);
-                        notFull.signalAll();
-                    }catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }finally {
-                        lock.unlock();
+                    }
                     }
                 }
-            }
-        }).start();
+            }).start();
+        }
+
+        for(int k=0;k<5;k++){
+            new Thread(new Runnable() {
+                public void run() {
+                    while(true){
+                        lock.lock();
+                        try {
+                            while(basket.size()==0){
+                                notEmpty.await();
+                            }
+                            Bread bread = basket.remove(0);
+                            System.out.println(Thread.currentThread().getName()+"remove:"+bread);
+                            notFull.signalAll();
+                        }catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }finally {
+                            lock.unlock();
+                        }
+                    }
+                }
+            }).start();
+        }
     }
 
 
